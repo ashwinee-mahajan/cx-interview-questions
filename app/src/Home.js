d1e436eafbf214da06cbe5297f4586400dff4782
ashwinee-mahajan <56250948+ashwinee-mahajan@users.noreply.github.com>
2019-11-08 12:10:36 +0000

    task1 completed

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,55 +1,34 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import SideBar from './SideBar';
+import React, { useState } from 'react';
 import Container from './Container';
-import Loading from './Loading';
 import './styles/Home.scss';
 import axios from 'axios';
 import { getRandomNumbers } from './utility/randomNumber';
 
 export default function Home() {
 
-    const [pokemonList, setPokemonList] = useState([]);
     const [pokemonDetails, setPokemonDetails] = useState({});
-    const [seletedPokemonIndex, setSelectedPokemonIndex] = useState(0);
-    const [isLoading, setLoading] = useState(true);
-    useEffect(() => {
-        const loadData = async (randomNumbers) => {
-            const uri = `https://pokeapi.co/api/v2/pokemon/`;
-            try {
-                setLoading(true);
-                const response = await axios.all(randomNumbers.map((number) => {
-                    return axios.get(`${uri}${number}`)
-                }));
-                setPokemonList(response);
-                setPokemonDetails(response[0].data);
-                setLoading(false);
-            } catch (error) {
-                setLoading(false);
-                console.log(error);
-            }
-        }
-        const randomNumbers = getRandomNumbers(10, 1, 151);
-        loadData(randomNumbers)
-    }, [])
 
-    const onClickPokemon = useCallback((data, index) => {
-        setPokemonDetails(data)
-        setSelectedPokemonIndex(index)
-    }, [setPokemonDetails])
+    const loadData = async (randomNumber) => {
+        const uri = `https://pokeapi.co/api/v2/pokemon/`;
+        try {
+            const response = await axios.get(`${uri}${randomNumber}`)
+            setPokemonDetails(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const loadPokemon = () => {
+        const randomNumbers = getRandomNumbers(1, 1, 151);
+        loadData(randomNumbers[0])
+    }
 
     return (
         <main className="Main">
-            {
-                isLoading ? <Loading /> : null
-            }
-            <SideBar
-                pokemonList={pokemonList}
-                onClickPokemon={onClickPokemon}
-                seletedPokemonIndex={seletedPokemonIndex} />
+            <button className="MainButton" onClick={loadPokemon}>Load Pokemon</button>
             {
                 Object.entries(pokemonDetails).length !== 0 ? <Container pokemonDetails={pokemonDetails} /> : null
             }
-
         </main>
     )
 }
